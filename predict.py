8b2a3a682a22ba27e539f45bc469df251ce1ba1c
Chenxi <chenxi.whitehouse@gmail.com>
2022-09-29 14:23:56 +0100

    Update predict.py

--- predict.py
+++ predict.py
@@ -2,14 +2,14 @@ import sys
 import torch
 import tempfile
 import numpy as np
-from pathlib import Path
 from PIL import Image
-import cog
+from cog import BasePredictor, Input, Path
 
 sys.path.append("ru-dalle")
 from rudalle.realesrgan.model import RealESRGAN
 
-class Predictor(cog.Predictor):
+
+class Predictor(BasePredictor):
     def setup(self):
         device = torch.device("cuda:0")
         scales = [2, 4, 8]
@@ -22,12 +22,14 @@ class Predictor(cog.Predictor):
 
     def predict(
         self,
-        image: Path = cog.Input(description="Input image", type=Path),
-        scale: int = cog.Input(description="Choose up-scaling factor", default=4, options=[2, 4, 8]),
+        image: Path = Input(description="Input image"),
+        scale: int = Input(
+            description="Choose up-scaling factor", default=4, choices=[2, 4, 8]
+        ),
     ) -> Path:
         realesrgan = self.models[scale]
-        input_image = Image.open(str(image))     
-        input_image = input_image.convert('RGB')
+        input_image = Image.open(str(image))
+        input_image = input_image.convert("RGB")
         with torch.no_grad():
             print("Up-scaling!")
             sr_image = realesrgan.predict(np.array(input_image))
